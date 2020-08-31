03abbf41e2405e54d6bd91870694ff3f27f222ec
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-08-31 13:55:01 +0200

    Add files via upload

---
+++ TestProject/TestProjectTests/SetUpFixture.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestProjectTests
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void SetUp() 
+        {
+            Console.WriteLine("It's SetUpFixture SetUp");
+        }
+        
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("It's SetUpFixture TearDown");
+        }
+    }
+}
---
+++ TestProject/TestProjectTests/Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestProjectTests
+{
+    public class Test
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("It's Test SetUp");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("It's Test TearDown");
+        }
+
+        [Test]
+        public void SomeTest()
+        {
+            Console.WriteLine("Hello from SomeTest");
+        }
+    }
+}
